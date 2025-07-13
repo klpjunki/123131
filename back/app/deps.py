c523376f3f0c2d6725c5ccafe5a3fe1c64afdd3b
diman <meis648@yandex.ru>
2025-07-13 17:04:41 +0300

    first commit

--- back/app/deps.py
+++
@@ -1,7 +0,0 @@
-from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.database import async_session_maker  
-
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:  
-        yield session
--- fornd/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const api = axios.create({
-  baseURL: import.meta.env.PROD
-      ? "https://tank-coin.ru/api"
-      : "/api"
-});
-
-export default api;
